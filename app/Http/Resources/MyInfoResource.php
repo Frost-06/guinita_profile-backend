d97b0997162918aa2d0877e7053db2ab6f2e3e63
Fros-06 <andredulaguinita@gmail.com>
2021-04-28 13:23:20 +0800

    first commit

---
+++ app/Http/Resources/MyInfoResource.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MyInfoResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+        return [
+            'id'                             => $this.id,
+            'imglink'                        => $this.imglink,
+            'firstname'                      => $this.firstname,
+            'mi'                             => $this.mi,
+            'lastname'                       => $this.lastname,
+            'email'                          => $this.email,
+            'address'                        => $this.address,
+            'phonenumber'                    => $this.phonenumber,
+            'description'                    => $this.description,
+            'age'                            => $this.age,
+            'birthday'                       => $this.birthday,
+            'gender'                         => $this.gender,
+            'status'                         => $this.status,
+            'religion'                       => $this.status,
+            'educcollegeprogram'           => $this.educcollegeprogram,
+            'educcollegeschool'            => $this.educcollegeschool,
+            'educcollegedate'              => $this.educcollegedate,
+            'educseniorhighschool'        => $this.educseniorhighschool,
+            'educseniorhighdate'          => $this.educseniorhighdate,
+            'educjuniorhighschool'        => $this.educjuniorhighschool,
+            'educjuniorhighdate'          => $this.educjuniorhighdate,
+            'educprimaryschool'            => $this.educprimaryschool,
+            'educprimarydate'              => $this.educprimarydate,
+        ];
+    }
+}
---
+++ database/migrations/2021_04_27_081027_create_myinfo_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMyinfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('myinfo', function (Blueprint $table) {
+            $table->id();
+            $table->string('imglink');
+            $table->string('firstname');
+            $table->string('mi');
+            $table->string('lastname');
+            $table->string('email');
+            $table->string('address');
+            $table->string('phonenumber');
+            $table->longText('description');
+            $table->string('age');
+            $table->string('birthday');
+            $table->string('gender');
+            $table->string('status');
+            $table->string('religion');
+            $table->string('educcollegeprogram');
+            $table->string('educcollegeschool');
+            $table->string('educcollegedate');
+            $table->string('educseniorhighschool');
+            $table->string('educseniorhighdate');
+            $table->string('educjuniorhighchool');
+            $table->string('educjuniorhighdate');
+            $table->string('educprimaryschool');
+            $table->string('educprimarydate');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('myinfo');
+    }
+}
